43dd917b03b04ef4c029ee133ddd3a194d9ce737
phillip-hlapa <philliphlapa2015@gmail.com>
2022-09-11 20:31:05 +0200

    added profile

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -2,7 +2,7 @@
 
  export const environment = {
     taskTrackerBackendService: {
-        dev: "https://viridian-wildebeest-hat.cyclic.app/ynitapi/my-task-tracker/",
-        prod: "http://localhost:1995/ynitapi/my-task-tracker/"
+        prod: "https://viridian-wildebeest-hat.cyclic.app/ynitapi/my-task-tracker/",
+        dev: "http://localhost:1995/ynitapi/my-task-tracker/"
     }
 }
